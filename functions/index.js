6be17040e8054765373292b6a07bd8cd7fe976cb
Jakob Anderson <janderson@familysearch.org>
2017-12-17 00:03:08 -0700

    working offline, dedupe, and started merge new articles

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const feedUtils = require('./feed-utils');
 const sources = require('./sources');
-const { makeArticle, getHash } = require('./article');
+const { makeArticle } = require('./article');
 
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
@@ -10,6 +10,12 @@ const admin = require('firebase-admin');
 
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
+
+// save a single article
+function saveArticle(article) {
+  const docRef = db.collection('publicArticles').doc(article.id);
+  return docRef.set(article);
+}
 
 exports.getFeedContent = functions.https.onRequest((req, res) => {
   feedUtils
@@ -23,12 +29,7 @@ exports.getFeedContent = functions.https.onRequest((req, res) => {
           labels: item.labels,
         })))
     .then((articles) => {
-      const dbProms = articles.map((article) => {
-        // content-based id as doc name to keep articles unique in db
-        const id = getHash(article);
-        const docRef = db.collection('publicArticles').doc(id);
-        return docRef.set(article);
-      });
+      const dbProms = articles.map(article => saveArticle(article));
       return Promise.all(dbProms)
         .then(results => res.status(200).send(results))
         .catch(err => res.status(500).send(err));
