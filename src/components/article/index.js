31a46c25030e80d3b76c1a3fab7e8d74770f1c54
Jakob Anderson <spacerockzero@users.noreply.github.com>
2018-03-24 20:58:24 -0600

    new backend server, imgs work now (#21)

--- src/components/article/index.js
+++ src/components/article/index.js
@@ -2,19 +2,25 @@ import { h, Component } from 'preact';
 import Card from 'preact-material-components/Card';
 import 'preact-material-components/Card/style.css';
 import 'preact-material-components/Button/style.css';
+import LazyLoad from 'react-lazy-load';
 // import style from './style';
 
 export default class Article extends Component {
 	render(props, state) {
-		const img = (
-			<a href={props.link}>
-				<img src={props.image} width="100" />
-			</a>
-		);
+		const img =
+			props.opengraph && props.opengraph.image ? (
+				<a href={props.link}>
+					<LazyLoad offsetTop={100}>
+						<img src={props.opengraph.image} width="100" />
+					</LazyLoad>
+				</a>
+			) : (
+				''
+			);
 		return (
 			<article className="article" data-id={props.id}>
 				<Card>
-					{props.image ? img : null}
+					{props.opengraph && props.opengraph.image ? img : null}
 					<div class="text-body">
 						<Card.Primary>
 							<Card.Subtitle>
@@ -22,7 +28,7 @@ export default class Article extends Component {
 							</Card.Subtitle>
 							<Card.Title>{props.feedsrc}</Card.Title>
 						</Card.Primary>
-						<Card.SupportingText>{new Date(props.createdOn).toDateString()}</Card.SupportingText>
+						{/* <Card.SupportingText>{new Date(props.createdOn).toDateString()}</Card.SupportingText> */}
 					</div>
 				</Card>
 			</article>
