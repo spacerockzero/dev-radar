2c7cbdc19eb11f55e554c742860920029dfa2877
Jakob Anderson <spacerockzero@users.noreply.github.com>
2018-05-12 18:51:50 -0600

    trimmed some fat (#31)
    * update articles on load better now
    
    * move things around
    
    * fixed no-content init bug
    
    * trimmed the fat

--- src/components/article/index.js
+++ src/components/article/index.js
@@ -1,16 +1,11 @@
 import { h, Component } from 'preact';
-import Card from 'preact-material-components/Card';
-import 'preact-material-components/Card/style.css';
-// import 'preact-material-components/Button/style.css';
-// import LazyLoad from 'react-lazy-load';
-// import style from './style';
 
 export default class Article extends Component {
 	render(props, state) {
 		const debugArticle = <pre>{JSON.stringify(props, null, 2)}</pre>;
 		const date = new Date(props.createdOn);
 		const dateString = `${date.toDateString()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}.${date.getMilliseconds()}`;
-		const dateEl = <Card.SupportingText className="article-date">{dateString}</Card.SupportingText>;
+		const dateEl = <div className="article-date">{dateString}</div>;
 		// const img =
 		// 	props.opengraph && props.opengraph.image ? (
 		// 		<a href={props.link}>
@@ -23,27 +18,21 @@ export default class Article extends Component {
 		// 	);
 		const labelList = props.labels.map(label => <span>{label}</span>);
 		return (
-			<article className="article" data-id={props.id}>
-				<Card>
-					{props.debug === true ? debugArticle : null}
-					{/* {props.opengraph && props.opengraph.image ? img : null} */}
-					<div class="text-body">
-						<Card.Primary>
-							<Card.Subtitle>
-								<a href={props.link}>{props.title}</a>
-							</Card.Subtitle>
-							{/* <Card.Title className="labels">
-								{props.labels && props.labels.length > 0 ? (
-									<div className="labels">{labelList}</div>
-								) : null}
-							</Card.Title> */}
-							<Card.Title>{props.feedsrc}</Card.Title>
-						</Card.Primary>
-
-						{props.debug === true ? dateEl : null}
+			<a href={props.link} className="article" data-id={props.id}>
+				{props.debug === true ? debugArticle : null}
+				{/* {props.opengraph && props.opengraph.image ? img : null} */}
+				<div class="text-body">
+					<div className="title" href={props.link}>
+						{props.title}
 					</div>
-				</Card>
-			</article>
+					{/* {props.labels && props.labels.length > 0 ? (
+            <div className="labels">{labelList}</div>
+          ) : null} */}
+					<div className="feedsrc">{props.feedsrc}</div>
+
+					{props.debug === true ? dateEl : null}
+				</div>
+			</a>
 		);
 	}
 }
